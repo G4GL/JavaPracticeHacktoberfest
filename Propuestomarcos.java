ca59d0e73b1e3bd61d7ea7c5877591de6878b5bb
G4GL <43782075+G4GL@users.noreply.github.com>
2018-10-15 12:22:18 +0200

    ola como tamo

---
+++ Propuestomarcos.java
@@ -0,0 +1,16 @@
+
+public class Propuesto1 {
+	
+	static int n1=50;
+	public static void main(String[] args)	{
+	int n2=30, suma=0, n3=10;
+	suma=n1+n2;
+	System.out.println("LA SUMA ES: " + suma);
+	suma=suma+n3;
+	//suma+=n3;
+	System.out.println(suma);
+	}
+	
+	//** Fin código****
+
+}
