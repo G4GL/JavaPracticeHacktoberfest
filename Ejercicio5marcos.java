19b16b7fef2b65ce0052bc0ea5bab5c2ddcbc7ac
G4GL <43782075+G4GL@users.noreply.github.com>
2018-10-15 12:16:15 +0200

    Add files via upload

---
+++ Ejercicio5marcos.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int edad=0;
+		int nivel_de_estudios=0;
+		int ingresos=0;
+		boolean jasp=false;
+		
+		System.out.println("edad: ");
+		edad=sc.nextInt();
+		
+		System.out.println("estudios: ");
+		nivel_de_estudios=sc.nextInt();
+		
+		System.out.println("ingresos: ");
+		ingresos=sc.nextInt();
+		
+		
+		jasp = ((edad<=28) && (nivel_de_estudios >3) && (ingresos>28000));
+				
+		System.out.println("CHAVAL TU JASP ES "+jasp);
+		
+		
+	}
+
+}
